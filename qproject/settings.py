b71c1574fa2c99acd22d9ee64f663ddcb47445df
Siddhesh Pardeshi <siddheshpardeshi2@gmail.com>
2022-12-24 09:56:41 +0530

    for heroku

--- qproject/settings.py
+++ qproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '9rw6%cfnmew3m6&pd7yjg*qapoyjadyv!)!_otnxicgl(oxn4k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- qproject/urls.py
+++ qproject/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
+from . import settings
+from django.views.static import serve
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls'), name="blog")
+    path('blog/',include('blog.urls'), name="blog"),
+
+    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
